86f8a691b067221dd8687b957fdbcf5ea7554c36
PainTrainee <vtec8197@gmail.com>
2024-01-21 15:24:44 +0700

    Product: CRUD

--- ProductWeb/Controllers/ProductController.cs
+++ ProductWeb/Controllers/ProductController.cs
@@ -1,27 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using ProductWeb.ViewModels;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ProductWeb.Controllers
 {
     public class ProductController : Controller
     {
         private readonly ProductContext productContext;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public ProductController(ProductContext productContext)
+        public ProductController(ProductContext productContext,IWebHostEnvironment webHostEnvironment)
         {
             this.productContext = productContext;
+            this.webHostEnvironment = webHostEnvironment;
         }
         public IActionResult Index()
         {
             var products = productContext.Products.Include(p => p.Category).ToList();
-            return View(products);
+            foreach (var item in products)
+            {
+                if (item.ImageUrl != null)
+                    item.ImageUrl = SD.ProductPath + "\\" + item.ImageUrl;
+            }
+            return View(products.OrderByDescending(p=>p.Id));
         }
         public IActionResult UpCreate(int? id)
         {
             var productVM = new ProductVM()
             {
-                Product = new(),
+                Product = new()
+                {
+                    Name = "TestProduct",
+                    Price = 1,
+                    Description = "Test Description"
+                },
                 CategoryList = productContext.Categories.Select(item => new SelectListItem
                 {
                     Text = item.Name,
@@ -43,6 +56,37 @@ namespace ProductWeb.Controllers
         [HttpPost]
         public IActionResult UpCreate(ProductVM productVM)
         {
+            if(!ModelState.IsValid) { return View(productVM); }
+            #region Image Management
+            string wwwRootPath = webHostEnvironment.WebRootPath;
+            var file = productVM.File;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString();
+                var extension = Path.GetExtension(file.FileName);
+
+                var uploads = wwwRootPath + SD.ProductPath; //wwwroot\images\product
+                if (!Directory.Exists(uploads))
+                    Directory.CreateDirectory(uploads);
+                //กรณีมีรูปภาพเดิมตอ้งลบทิ้งก่อน
+                if (productVM.Product.ImageUrl != null)
+                {
+                    var oldImagePath = Path.Combine(uploads, productVM.Product.ImageUrl);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+                //บันทึกรุปภาพใหม่
+
+                using (var fileStreams = new FileStream(Path.Combine(uploads,fileName + extension), FileMode.Create))
+                {
+                    file.CopyTo(fileStreams);
+                }
+
+                productVM.Product.ImageUrl = fileName + extension;
+            }
+            #endregion Image Management
             var id = productVM.Product.Id;
             if (id != 0)
             {
@@ -66,6 +110,15 @@ namespace ProductWeb.Controllers
                 TempData["Message"] = "Not Found.";
                 return RedirectToAction(nameof(Index));
             }
+            if (product.ImageUrl != null)
+            {
+                var oldImagePath = webHostEnvironment.WebRootPath + SD.ProductPath + "\\" + product.ImageUrl;
+                if (System.IO.File.Exists(oldImagePath))
+                {
+                    System.IO.File.Delete(oldImagePath);
+                }
+            }
+            //wwwroot\images\products\Name.jpg
             productContext.Remove(product);
             productContext.SaveChanges();
             TempData["Message"] = "Delete Successfully.";
--- ProductWeb/ViewModels/ProductVM.cs
+++ ProductWeb/ViewModels/ProductVM.cs
@@ -8,5 +8,7 @@ namespace ProductWeb.ViewModels
         public Product Product { get; set; }
         [ValidateNever]
         public IEnumerable<SelectListItem> CategoryList { get; set;}
+        [ValidateNever]
+        public IFormFile File { get; set; }
     }
 }
